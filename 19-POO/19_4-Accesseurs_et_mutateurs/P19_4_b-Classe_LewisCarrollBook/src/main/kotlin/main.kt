122da507e3ac62c3de4bdcc1e6b3ee3d940b900d
EvanR <evan.rieu@gmail.com>
2024-09-24 11:58:41 +0200

    Exo 24/09

--- 19-POO/19_4-Accesseurs_et_mutateurs/P19_4_b-Classe_LewisCarrollBook/src/main/kotlin/main.kt
+++ 19-POO/19_4-Accesseurs_et_mutateurs/P19_4_b-Classe_LewisCarrollBook/src/main/kotlin/main.kt
@@ -1,10 +1,23 @@
-// write your class here
-
+class LewisCarrollBook {
+    var name: String = ""
+    get() {
+        return field
+    }
+        set(value) =
+}
 
 fun main() {
-    val alicesAdventuresInWonderland = LewisCarrollBook()
-    alicesAdventuresInWonderland.name = "Alice's Adventures in Wonderland"
+//    val alicesAdventuresInWonderland = LewisCarrollBook()
+//    alicesAdventuresInWonderland.name = "Alice's Adventures in Wonderland"
+//
+//    println(alicesAdventuresInWonderland.author)
+//    alicesAdventuresInWonderland.price = 10
 
-    println(alicesAdventuresInWonderland.author)
-    alicesAdventuresInWonderland.price = 10
+    val lewisCarrollBook = LewisCarrollBook()
+    var bookName = lewisCarrollBook.name
+    lewisCarrollBook.name = "Alice's Adventures in Wonderland"
+    bookName = lewisCarrollBook.name
+    var authorName = lewisCarrollBook.author
+    var bookPrice = lewisCarrollBook.price
+    lewisCarrollBook.price = 1500
 }
